eda8fd6d1d6c206c4344129d56dc3f1d388d50c8
Gian Hancock <gian@vldata.com.au>
2022-12-14 06:12:28 +1100

    Implement day 3, part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,8 +1,12 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::fs::File;
 use std::io::{self, BufRead};
 
 fn main() {
+    part_2();
+}
+
+fn part_1() {
     // # Create line iterator over input file
     let input_file = File::open("input.txt").unwrap();
     let line_iterator = io::BufReader::new(input_file).lines();
@@ -37,6 +41,35 @@ fn main() {
             }
             current_char += 1;
         }
+    }
+    println!("{}", priority_sum);
+}
+
+fn part_2() {
+    // # Create line iterator over input file
+    let input_file = File::open("input.txt").unwrap();
+    let mut line_iterator = io::BufReader::new(input_file).lines();
+
+    let mut priority_sum = 0;
+    'outer: loop {
+        let mut char_counts = HashMap::new();
+        for _ in 0..3 {
+            let mut line_chars = HashSet::new();
+            let line = match line_iterator.next() {
+                Some(line) => line.unwrap(),
+                None => break 'outer
+            };
+            for c in line.chars() {
+                if !line_chars.contains(&c) {
+                    char_counts.entry(c).and_modify(|c| *c += 1).or_insert(1);
+                }
+                line_chars.insert(c);
+            }
+        }
+        let mut common_chars = char_counts.iter().filter(|(_k, v)| **v == 3);
+        let (common_char, _) = common_chars.next().unwrap();
+        assert!(common_chars.count() == 0);
+        priority_sum += char_to_priority(*common_char)
     }
     println!("{}", priority_sum);
 }
