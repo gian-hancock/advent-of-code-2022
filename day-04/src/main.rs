4af388aef29cf552db0c0ab8787185ea616ce6b4
Gian Hancock <gian.hancock@outlook.com>
2022-12-18 13:17:46 +1100

    Implement day 4 and 5

---
+++ day-04/src/main.rs
@@ -0,0 +1,55 @@
+use std::io::{BufReader, BufRead};
+use std::fs::File;
+use std::iter::Peekable;
+
+fn main() {
+    let input_reader = BufReader::new(File::open("input.txt").unwrap());
+    
+    let mut contained_range_count = 0;
+    let mut overlapping_range_count = 0;
+    for line in input_reader.lines() {
+        let line = line.unwrap();
+        let (mut range_1, mut range_2)  = parse_line(&line);
+
+        // Order ranges so that range_1 is the leftmost lower bound
+        if range_2.0 <= range_1.0 {
+            (range_1, range_2) = (range_2, range_1);
+        }
+
+        // Determine of one range contains the other
+        if range_1.0 == range_2.0 || range_2.1 <= range_1.1 {
+            contained_range_count += 1;
+        }
+
+        // Determine if the ranges overlap
+        if range_1.1 >= range_2.0 {
+            overlapping_range_count += 1;
+        }
+    }
+
+    println!("Part 1: {}", contained_range_count);
+    println!("Part 2: {}", overlapping_range_count);
+}
+
+fn parse_line(line: &String) -> ((i32, i32), (i32, i32)) { 
+    let mut chars = line.chars().peekable();
+
+    let range_1_lower = next_int(&mut chars);
+    assert!(chars.next().unwrap() == '-');
+    let range_1_upper = next_int(&mut chars);
+
+    assert!(chars.next().unwrap() == ',');
+
+    let range_2_lower = next_int(&mut chars);
+    assert!(chars.next().unwrap() == '-');
+    let range_2_upper = next_int(&mut chars);
+    ((range_1_lower, range_1_upper,), (range_2_lower, range_2_upper,),)
+}
+
+fn next_int<T: Iterator<Item = char>>(chars: &mut Peekable<T>) -> i32 {
+    let mut result = String::new();
+    while matches!(chars.peek(), Some(c) if c.is_ascii_digit()) {
+        result.push(chars.next().unwrap());
+    }
+    result.parse().unwrap()
+}
---
+++ day-05/src/main.rs
@@ -0,0 +1,142 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::iter::Peekable;
+
+fn main() {
+    // part_1();
+    part_2();
+}
+
+fn part_1() {
+    let input_file = File::open("input.txt").unwrap();
+    let mut line_iterator = io::BufReader::new(input_file).lines();
+
+    let mut stacks = parse_stacks(&mut line_iterator);
+    println!("Stacks initial state: {:?}", stacks);
+
+    // # Consume blank line which separates initial stack state from crane instructions
+    line_iterator.next();
+
+    // # Manipulate stacks from file input
+    let stack_op_iter = line_iterator.map(|line| parse_stack_op(line.unwrap()));
+    for stack_op in stack_op_iter {
+        for i in 0..stack_op.count {
+            let t = stacks[stack_op.from].pop().unwrap();
+            stacks[stack_op.to].push(t);
+        }
+    }
+    println!("{:?}", stacks);
+}
+
+fn part_2() {
+    let input_file = File::open("input.txt").unwrap();
+    let mut line_iterator = io::BufReader::new(input_file).lines();
+
+    let mut stacks = parse_stacks(&mut line_iterator);
+    println!("Stacks initial state: {:?}", stacks);
+
+    // # Consume blank line which separates initial stack state from crane instructions
+    line_iterator.next();
+
+    // # Manipulate stacks from file input
+    let stack_op_iter = line_iterator.map(|line| parse_stack_op(line.unwrap()));
+    for stack_op in stack_op_iter {
+        println!("{:?}", stacks);
+        let new_len = stacks[stack_op.from].len() - stack_op.count;
+        let stack_to_move: Vec<char> = stacks[stack_op.from].drain(new_len..).collect();
+        stacks[stack_op.to].extend(stack_to_move);
+    }
+    println!("{:?}", stacks);
+}
+
+fn parse_stacks<T: Iterator<Item = Result<String, std::io::Error>>>(lines: &mut T) -> Vec<Vec<char>> {
+    let mut stacks = vec![];
+    'outer: loop {
+        let line = lines.next().unwrap().unwrap();
+        // Iterate create IDs in line. If a digit is encountered, we have reached the end of the crate IDs
+        for (i, c) in line.chars().skip(1).step_by(4).enumerate() {
+            if c.is_ascii_digit() { break 'outer; }
+            if c == ' ' { continue; }
+            while i >= stacks.len() { stacks.push(Vec::new()); }
+            stacks[i].push(c);
+        }
+    }
+    for stack in &mut stacks {
+        stack.reverse();
+    }
+    stacks
+}
+
+/// Parses a stack operation of the form "move 2 from 8 to 4"
+fn parse_stack_op(line: String) -> StackOperation {
+    let mut scanner = Scanner::new(line.chars());
+    scanner.next_word();
+    let count = scanner.next_int();
+    scanner.next_word();
+    let from = scanner.next_int();
+    scanner.next_word();
+    let to = scanner.next_int();
+    let result = StackOperation { 
+        count: count as usize, 
+        from: (from - 1) as usize,
+        to: (to - 1) as usize, 
+    };
+    result
+}
+
+#[derive(Debug)]
+struct StackOperation {
+    count: usize,
+    from: usize,
+    to: usize,
+}
+
+struct Scanner<T: Iterator<Item = char>> {
+    chars: Peekable<T>,
+}
+
+impl<T: Iterator<Item = char>> Scanner<T> {
+    fn new(chars: T) -> Scanner<T> where T: Iterator<Item = char> {
+        Scanner { chars: chars.peekable() }
+    }
+
+    fn next_word(&mut self) -> () {
+        loop {
+            let c = self.chars.next();
+            match c {
+                None => break,
+                Some(c) => {
+                    if c.is_ascii_whitespace() { break }
+                },
+            }
+        }
+    }
+
+    fn next_int(&mut self) -> i32 {
+        let mut value = String::new();
+        loop {
+            let c = self.chars.next();
+            match c {
+                None => break,
+                Some(c) => {
+                    if c.is_ascii_digit() {
+                        value.push(c)
+                    }
+                    else {
+                        break;
+                    }
+                },
+            }
+        }
+        value.parse().unwrap()
+
+    }
+}
+
+
+/*WVWPBNGZ WVWPBNGZW
+VLCWHTDSZ
+[['G', 'W', 'N', 'C', 'V'], ['L'], ['C'], ['W'], ['M', 'B', 'N', 'P', 'W', 'B', 'V', 'J', 'B', 'T', 'M', 'D', 'C', 'F', 'T', 'B', 'C', 'W', 'N', 'H'], 
+['F', 'T'], ['N', 'C', 'W', 'D', 'Z', 'G', 'B', 'V', 'S', 'T', 'S', 'D'], ['S'], ['M', 'G', 'P', 'R', 'L', 'F', 'Q', 'G', 'S', 'J', 'Z', 'V', 'Z']]
+
+ */
