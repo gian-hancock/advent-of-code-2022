fb79020a25dd2dad50725318b37a018c7fc15102
Gian Hancock <gian.hancock@outlook.com>
2022-12-23 20:39:55 +1100

    Complete day 9, part 2

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -3,29 +3,30 @@ use std::fs;
 use std::io;
 use std::io::BufRead;
 
+const KNOT_COUNT: usize = 10;
 fn main() {
     // # Parse file into series of Movements
     let movements = io::BufReader::new(fs::File::open("input.txt").unwrap())
         .lines()
         .map(|l| parse_line(l.unwrap()));
 
-    let mut pos_head = Position {x: 0, y: 0};
-    let mut pos_tail = Position {x: 0, y: 0};
+    let mut knot_positions = [Position { x: 0, y: 0 }; KNOT_COUNT];
     let mut tail_positions: HashSet<Position> = HashSet::new();
     for movement in movements {
-        println!("{:?}", movement);
-        for count in 0..movement.count {
-            pos_head.x += movement.dx;
-            pos_head.y += movement.dy;
-            if (pos_head.x - pos_tail.x).abs() > 1 {
-                pos_tail.x = (pos_head.x + pos_tail.x) / 2;
-                pos_tail.y = pos_head.y;
-            } else if (pos_head.y - pos_tail.y).abs() > 1 {
-                pos_tail.y = (pos_head.y + pos_tail.y) / 2;
-                pos_tail.x = pos_head.x;
+        for _ in 0..movement.count {
+            knot_positions[0].x += movement.dx;
+            knot_positions[0].y += movement.dy;
+            for knot_idx in 1..knot_positions.len() {
+                let cur = knot_positions[knot_idx];
+                let prev = knot_positions[knot_idx - 1];
+                let dx = prev.x - cur.x;
+                let dy = prev.y - cur.y;
+                if dx.abs() > 1 || dy.abs() > 1 {
+                    knot_positions[knot_idx].x += dx.signum();
+                    knot_positions[knot_idx].y += dy.signum();
+                }
             }
-            println!("  H: {:?}, T: {:?}", pos_head, pos_tail);
-            tail_positions.insert(pos_tail);
+            tail_positions.insert(*knot_positions.last().unwrap());
         }
     }
     println!("Tail occupied {} unique positions", tail_positions.len());
@@ -34,8 +35,8 @@ fn main() {
 fn parse_line(line: String) -> Movement {
     let mut tokens = line.split(' ');
     let direction = match tokens.next() {
-        Some("U") => (0, 1),
-        Some("D") => (0, -1),
+        Some("U") => (0, -1),
+        Some("D") => (0, 1),
         Some("L") => (-1, 0),
         Some("R") => (1, 0),
         _ => panic!(),
